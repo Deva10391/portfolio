6528c7c6fd8a6b31357a4037e217de81e89c2c4e
deva40211 <devashish15262@gmail.com>
2024-01-31 14:04:08 +0530

    update

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Download() {
     return (
         <div className="btn-con">
-            <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-835be.appspot.com/o/Devashish-FullStack%20developer.pdf?alt=media&token=09cc9bd2-4396-445f-b6a3-afbe141cb397" className="main-btn">
+            <a href="https://drive.google.com/file/d/1gOk-l5hgV0VP-5iRclE5iHgDPbcByDlv/view?usp=sharing" className="main-btn">
                 <span className="btn-text">Download CV</span>
                 <span className="btn-icon"><i className="fas fa-download"></i></span>
             </a>
